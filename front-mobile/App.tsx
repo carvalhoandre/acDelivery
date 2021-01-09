a603c9b3313e22df7d7fe43f6c729acfeb3a5c19
André Carvalho <andre_carvalho0@live.com>
2021-01-09 16:28:37 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
-import { StyleSheet, Text, View } from 'react-native';
-import Header  from './src/Header';
+import { StyleSheet, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 
 export default function App() {
@@ -20,8 +19,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
